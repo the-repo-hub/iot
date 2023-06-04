6a26ad0c7366b0856bb7ee0ddb6f7e4f29e3ccd3
debian <debian@debian>
2023-06-05 01:55:06 +0300

    bad version

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # Training a ML model using CICIoT2023
-
 import pandas as pd
-import numpy as np
-import os
 from tqdm import tqdm
 import warnings
 from sklearn.linear_model import LogisticRegression
@@ -19,21 +16,7 @@ class IOT:
 
     def __init__(self):
         warnings.filterwarnings('ignore')
-        self.DATASET_DIRECTORY = 'C:\\CICIoT2023\\'
-
-        self.dict_2classes = {'DDoS-RSTFINFlood': 'Attack', 'DDoS-PSHACK_Flood': 'Attack', 'DDoS-SYN_Flood': 'Attack',
-                              'DDoS-UDP_Flood': 'Attack', 'DDoS-TCP_Flood': 'Attack', 'DDoS-ICMP_Flood': 'Attack',
-                              'DDoS-SynonymousIP_Flood': 'Attack', 'DDoS-ACK_Fragmentation': 'Attack',
-                              'DDoS-UDP_Fragmentation': 'Attack', 'DDoS-ICMP_Fragmentation': 'Attack',
-                              'DDoS-SlowLoris': 'Attack',
-                              'DDoS-HTTP_Flood': 'Attack', 'DoS-UDP_Flood': 'Attack', 'DoS-SYN_Flood': 'Attack',
-                              'DoS-TCP_Flood': 'Attack', 'DoS-HTTP_Flood': 'Attack', 'Mirai-greeth_flood': 'Attack',
-                              'Mirai-greip_flood': 'Attack', 'Mirai-udpplain': 'Attack', 'Recon-PingSweep': 'Attack',
-                              'Recon-OSScan': 'Attack', 'Recon-PortScan': 'Attack', 'VulnerabilityScan': 'Attack',
-                              'Recon-HostDiscovery': 'Attack', 'DNS_Spoofing': 'Attack', 'MITM-ArpSpoofing': 'Attack',
-                              'BrowserHijacking': 'Attack', 'Backdoor_Malware': 'Attack', 'XSS': 'Attack',
-                              'Uploading_Attack': 'Attack', 'SqlInjection': 'Attack', 'CommandInjection': 'Attack',
-                              'DictionaryBruteForce': 'Attack', 'BenignTraffic': 'Benign'}
+        self.DATASET_DIRECTORY = '/home/debian/PycharmProjects/iot/'
 
         self.DDoS = {'DDoS-RSTFINFlood': 'Attack', 'DDoS-PSHACK_Flood': 'Attack', 'DDoS-SYN_Flood': 'Attack',
                      'DDoS-UDP_Flood': 'Attack', 'DDoS-TCP_Flood': 'Attack', 'DDoS-ICMP_Flood': 'Attack',
@@ -131,20 +114,6 @@ class IOT:
                            'Uploading_Attack': 'Benign', 'SqlInjection': 'Benign', 'CommandInjection': 'Benign',
                            'DictionaryBruteForce': 'Attack', 'BenignTraffic': 'Benign'}
 
-        self.dict_7classes = {'DDoS-RSTFINFlood': 'DDoS', 'DDoS-PSHACK_Flood': 'DDoS', 'DDoS-SYN_Flood': 'DDoS',
-                              'DDoS-UDP_Flood': 'DDoS', 'DDoS-TCP_Flood': 'DDoS', 'DDoS-ICMP_Flood': 'DDoS',
-                              'DDoS-SynonymousIP_Flood': 'DDoS', 'DDoS-ACK_Fragmentation': 'DDoS',
-                              'DDoS-UDP_Fragmentation': 'DDoS',
-                              'DDoS-ICMP_Fragmentation': 'DDoS', 'DDoS-SlowLoris': 'DDoS', 'DDoS-HTTP_Flood': 'DDoS',
-                              'DoS-UDP_Flood': 'DoS', 'DoS-SYN_Flood': 'DoS', 'DoS-TCP_Flood': 'DoS',
-                              'DoS-HTTP_Flood': 'DoS',
-                              'Mirai-greeth_flood': 'Mirai', 'Mirai-greip_flood': 'Mirai', 'Mirai-udpplain': 'Mirai',
-                              'Recon-PingSweep': 'Recon', 'Recon-OSScan': 'Recon', 'Recon-PortScan': 'Recon',
-                              'VulnerabilityScan': 'Recon', 'Recon-HostDiscovery': 'Recon', 'DNS_Spoofing': 'Spoofing',
-                              'MITM-ArpSpoofing': 'Spoofing', 'BenignTraffic': 'Benign', 'BrowserHijacking': 'Web',
-                              'Backdoor_Malware': 'Web', 'XSS': 'Web', 'Uploading_Attack': 'Web', 'SqlInjection': 'Web',
-                              'CommandInjection': 'Web', 'DictionaryBruteForce': 'BruteForce'}
-
         ### Importing Dataset
         self.df_sets = [k for k in os.listdir(self.DATASET_DIRECTORY) if k.endswith('.csv')]
         self.df_sets.sort()
@@ -169,42 +138,6 @@ class IOT:
             scaler.fit(pd.read_csv(self.DATASET_DIRECTORY + train_set)[self.X_columns])
         return scaler
 
-    def Scaning34(self):
-
-        scaler = self.make_scaler()
-
-        for train_set in tqdm(self.training_sets):
-            d = pd.read_csv(self.DATASET_DIRECTORY + train_set)
-            d[self.X_columns] = scaler.transform(d[self.X_columns])
-
-            self.model.fit(d[self.X_columns], d[self.y_column])
-
-        for test_set in tqdm(self.test_sets):
-            d_test = pd.read_csv(self.DATASET_DIRECTORY + test_set)
-            d_test[self.X_columns] = scaler.transform(d_test[self.X_columns])
-            self.y_test += list(d_test[self.y_column].values)
-            self.y_pred = list(self.model.predict(d_test[self.X_columns]))
-
-    def Scaning7(self):
-
-        scaler = self.make_scaler()
-
-        for train_set in tqdm(self.training_sets):
-            d = pd.read_csv(self.DATASET_DIRECTORY + train_set)
-            d[self.X_columns] = scaler.transform(d[self.X_columns])
-            new_y = [self.dict_7classes[k] for k in d[self.y_column]]
-            d[self.y_column] = new_y
-
-            self.model.fit(d[self.X_columns], d[self.y_column])
-
-        for test_set in tqdm(self.test_sets):
-            d_test = pd.read_csv(self.DATASET_DIRECTORY + test_set)
-            d_test[self.X_columns] = scaler.transform(d_test[self.X_columns])
-            new_y = [self.dict_7classes[k] for k in d_test[self.y_column]]
-            d_test[self.y_column] = new_y
-            self.y_test += list(d_test[self.y_column].values)
-            self.y_pred = list(self.model.predict(d_test[self.X_columns]))
-
     def scanning2(self, model):
         y_pred = []
         y_test = []
@@ -228,30 +161,134 @@ class IOT:
             y_test = list(d_test[self.y_column].values)
             y_pred = list(model.predict(d_test[self.X_columns]))
 
-        print('#####', model, '#####')
+        self.metrics(model, y_pred, y_test)
+
+    def metrics(self, model, y_pred, y_test):
+
+        print(f"##### {model} #####")
         print('confusion matrix:\n', confusion_matrix(y_pred, y_test))
         print('accuracy_score: ', accuracy_score(y_pred, y_test))
         print('recall_score: ', recall_score(y_pred, y_test, average='macro'))
         print('precision_score: ', precision_score(y_pred, y_test, average='macro'))
         print('f1_score: ', f1_score(y_pred, y_test, average='macro'))
 
-    def metrics(self, s):
-
-        print(f"##### {self.models[s]} #####")
-        print('confusion matrix:\n', confusion_matrix(self.y_pred, self.y_test))
-        print('accuracy_score: ', accuracy_score(self.y_pred, self.y_test))
-        print('recall_score: ', recall_score(self.y_pred, self.y_test, average='macro'))
-        print('precision_score: ', precision_score(self.y_pred, self.y_test, average='macro'))
-        print('f1_score: ', f1_score(self.y_pred, self.y_test, average='macro'))
-
     def run(self):
-
-        for num, model in enumerate(self.models[:2], start=0):
-            if num == 0:
-                continue
-
+        for num, model in enumerate(self.models, start=0):
             self.scanning2(model)
 
 
+from customtkinter import *
+from time import sleep
+import sys
+import threading
+
+
+class IotMock:
+    @staticmethod
+    def scanning2(model, attack):
+        print('starting...', model, attack)
+        for i in range(3):
+            sleep(1)
+            print(i)
+        print('doing...')
+        sleep(1)
+        print('done!')
+
+
+class TextRedirector:
+
+    def __init__(self, widget, tag="stdout"):
+        self.widget = widget
+        self.tag = tag
+        self.output = ''
+
+    def write(self, str):
+        self.output += str
+        self.widget.configure(state="normal")
+        self.widget.insert("end", str)
+        self.widget.configure(state="disabled")
+
+    def flush(self):
+        pass
+
+
+class App(CTk):
+    attacks = [
+        "BruteForce",
+        "Web",
+        "Spoofing",
+        "Recon",
+        "Mirai",
+        "DOS",
+        "DDOS",
+    ]
+    attack = attacks[0]
+
+    methods = [
+        "Логистическая регрессия",
+        "К - ближайших соседей",
+        "Дерево принятия решений",
+        "Наивная байесовская классификация",
+        "Метод случайного леса",
+        "Метод опорных векторов",
+    ]
+    method = methods[0]
+
+    iot = IotMock()
+
+    height = 600
+    width = 600
+    color = '#522878'
+
+    def __init__(self):
+        super().__init__()
+        self.title('IOT GUI')
+        # self.resizable(True, True)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
+        self.rowconfigure(0, weight=1)
+
+        self.left_frame = CTkFrame(self)
+        CTkLabel(self.left_frame, text='Лог:').grid(row=0, column=0)
+        self.text = CTkTextbox(self.left_frame)
+        self.text.grid(row=1, column=0)
+        self.left_frame.grid(row=0, column=0)
+
+        self.right_frame = CTkFrame(self)
+        CTkLabel(self.right_frame, text='Выбор атаки:').grid(row=0, column=0)
+        CTkOptionMenu(self.right_frame,
+                      values=self.attacks,
+                      command=lambda choice: self.choose_attack(choice),
+                      fg_color=self.color,
+                      button_color=self.color).grid(row=1, column=0)
+        CTkLabel(self.right_frame, text='Выбор метода:').grid(row=2, column=0)
+        CTkOptionMenu(self.right_frame,
+                      values=self.methods,
+                      command=lambda choice: self.choose_method(choice),
+                      fg_color=self.color,
+                      button_color=self.color).grid(row=3, column=0)
+        CTkLabel(self.right_frame, text='').grid(row=4, column=1)
+        CTkButton(self.right_frame, text='Сохранить лог', )
+        CTkButton(self.right_frame, text='Start', command=self.run, fg_color=self.color).grid(row=5, column=0)
+
+        self.right_frame.grid(row=0, column=1)
+        self.redirect = TextRedirector(self.text, "stdout")
+        sys.stdout = self.redirect
+
+    def save_log(self):
+        with open('log.txt', 'w') as file:
+            file.write(self.redirect.output)
+
+    def run(self):
+        threading.Thread(target=self.iot.scanning2, daemon=True, args=(self.method, self.attack)).start()
+
+    def choose_attack(self, choice):
+        self.attack = choice
+
+    def choose_method(self, choice):
+        self.method = choice
+
+
 if __name__ == '__main__':
-    IOT().run()
+    app = App()
+    app.mainloop()
